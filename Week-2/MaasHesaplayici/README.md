9a7f7f3cdb7ab63fdf4d5e001c91b70207a1b83c
Ali Osman Uysal <aliosmanuysall@gmail.com>
2024-03-03 21:19:05 +0300

    First Commit

---
+++ Week-2/MaasHesaplayici/README.md
@@ -0,0 +1,2 @@
+# Maaş Hesaplayıcı
+#### BY: ALİ OSMAN UYSAL - 2024
---
+++ Week-2/MaasHesaplayici/src/Employee.java
@@ -0,0 +1,70 @@
+import java.util.Calendar;
+
+public class Employee {
+    private String name;
+    private double salary;
+    private int workHours;
+    private int hireYear;
+
+    // Kurucu Metot
+    public Employee(String name, double salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    // Vergi Hesaplama Metodu
+    public double tax() {
+        if (salary > 1000) {
+            return 0.03 * salary;
+        } else {
+            return 0;
+        }
+    }
+
+    // Bonus Hesaplama Metodu
+    public double bonus() {
+        if (workHours > 40) {
+            int extraHours = workHours - 40;
+            return extraHours * 30;
+        } else {
+            return 0;
+        }
+    }
+
+    // Zam Hesaplama Metodu
+    public void raiseSalary() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int yearsWorked = currentYear - hireYear;
+
+        if (yearsWorked < 10) {
+            salary *= 1.05;
+        } else if (yearsWorked < 20) {
+            salary *= 1.10;
+        } else {
+            salary *= 1.15;
+        }
+    }
+
+    // Bilgileri String olarak döndüren Metod
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", workHours=" + workHours +
+                ", hireYear=" + hireYear +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        // Örnek Kullanım
+        Employee employee = new Employee("Ahmet Yılmaz", 1200, 45, 2010);
+        System.out.println("Vergi: " + employee.tax());
+        System.out.println("Bonus: " + employee.bonus());
+
+        System.out.println("Maaş Artışı Öncesi: " + employee);
+        employee.raiseSalary();
+        System.out.println("Maaş Artışı Sonrası: " + employee);
+    }
+}
