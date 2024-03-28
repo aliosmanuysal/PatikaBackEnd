f0828b195899f8d4d034489d99f59f8c691c5aea
Ali Osman Uysal <aliosmanuysall@gmail.com>
2024-03-29 01:50:13 +0300

    Initial commit

---
+++ Week-5/SQLWorks/SQL_3.sql
@@ -0,0 +1,22 @@
+--Country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+
+SELECT* FROM country
+WHERE country LIKE 'A%a';
+
+
+--Country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+
+SELECT country FROM country
+WHERE country LIKE '_____%n';
+
+
+--Film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+
+SELECT * FROM film
+WHERE title ILIKE '%T%T%T%T';
+
+
+--Film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length >90 AND rental_rate=2.99;
