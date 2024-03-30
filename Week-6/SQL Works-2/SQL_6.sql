52ce0eebbfbb82caf158ecc85d2647822d26188f
Ali Osman Uysal <aliosmanuysall@gmail.com>
2024-03-30 16:17:57 +0300

    Initial commit

---
+++ Week-6/SQL Works-2/SQL_6.sql
@@ -0,0 +1,22 @@
+-- 1-Film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+
+SELECT AVG(rental_rate)
+FROM film;
+
+-- 2-Film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+
+SELECT COUNT(title)
+FROM film
+WHERE title LIKE 'C%';
+
+-- 3-Film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+
+SELECT MAX(length)
+FROM film
+WHERE rental_rate = 0.99;
+
+-- 4-Film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+
+SELECT COUNT(DISTINCT replacement_cost)
+FROM film
+WHERE length > 150;
---
+++ Week-6/SQL_8.sql
@@ -0,0 +1,22 @@
+--1-Film tablosunda bulunan filmleri rating değerlerine göre gruplayın
+
+SELECT rating, COUNT(*) FROM film
+GROUP BY rating;
+ 
+--2-Film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+--3-Customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+
+--4-City tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+
+SELECT country_id, COUNT(country_id) FROM city  
+GROUP BY country_id
+ORDER BY COUNT(country_id) DESC
+LIMIT 1;
